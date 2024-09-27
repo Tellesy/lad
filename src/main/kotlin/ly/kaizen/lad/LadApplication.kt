1db4e904f3676f57138004eb2603037f324a575c
Tellesy <mtellesy@gmail.com>
2024-09-27 18:44:40 +0200

    Add Spring Boot project setup with Gradle support
    Configured Spring Boot application with Gradle build system, including Kotlin support and essential dependencies. Included Gradle wrapper scripts for both Windows and POSIX environments and a basic `.gitignore` file.

---
+++ src/main/kotlin/ly/kaizen/lad/LadApplication.kt
@@ -0,0 +1,11 @@
+package ly.kaizen.lad
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LadApplication
+
+fun main(args: Array<String>) {
+    runApplication<LadApplication>(*args)
+}
